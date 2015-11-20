bba384dffebb63b802c7fc1f6c82d4f545cc6bb7
wangjch <546700420@qq.com>
2015-11-20 17:48:58 +0800

    新增查询省市县列表的功能

--- src/com/coolweather/app/db/CoolWeatherOpenHelper.java
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -8,16 +8,16 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 
 	// Province建表语句
-	public static final String CREATE_PROVINCE = "create table Province("
-			+ "id interger primary key auto increment," + "province_name text,"
+	public static final String CREATE_PROVINCE = "create table Province ("
+			+ "id integer primary key autoincrement, " + "province_name text, "
 			+ "province_code text)";
 	// City建表语句
 	public static final String CREATE_CITY = "create table City("
-			+ "id interger primary key auto increment," + "city_name text,"
+			+ "id integer primary key autoincrement," + "city_name text,"
 			+ "city_code text," + "province_id integer)";
 	// County建表语句
 	public static final String CREATE_County = "create table County("
-			+ "id interger primary key auto increment," + "county_name text,"
+			+ "id integer primary key autoincrement," + "county_name text,"
 			+ "county_code text," + "city_id integer)";
 
 	public CoolWeatherOpenHelper(Context context, String name,
@@ -29,8 +29,8 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		// TODO Auto-generated method stub
-		db.execSQL(CREATE_PROVINCE); //创建Province表
-		db.execSQL(CREATE_CITY);	
+		db.execSQL(CREATE_PROVINCE); // 创建Province表
+		db.execSQL(CREATE_CITY);
 		db.execSQL(CREATE_County);
 
 	}
